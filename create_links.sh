722fc95b4104f081c88115f16170962c83564cad
linus.roepert <linus.roepert@gmail.com>
2019-01-06 01:23:45 +0100

    Added ranger config

--- create_links.sh
+++ create_links.sh
@@ -6,6 +6,7 @@ ln -s $dotfir/spacemacs ~/.spacemacs
 ln -s $dotdir/zshrc ~/.zshrc
 ln -s $dotdir/vimrc ~/.vim/vimrc
 ln -s $dotdir/Xresources ~/.Xresources
+ln -s $dotdir/ranger ~/.config/ranger/rc.conf
 
 #i3 stuff
 ln -s $dotdir/i3conf ~/.config/i3/config
